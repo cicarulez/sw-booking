2049cf9b495b1ff48514587ae293c8a6eac42287
DC <cappadavide@gmail.com>
2023-03-07 15:48:51 +0100

    feat(xlsx): added xlsx file generation

--- booking.ts
+++ booking.ts
@@ -1,4 +1,5 @@
 import * as fs from "fs";
+import XLSX from "xlsx";
 
 interface Worker {
     name: string;
@@ -51,3 +52,40 @@ for (let weekIndex = 0; weekIndex < NUMBER_OF_WEEKS; weekIndex++) {
 }
 
 console.log(booking); // for testing purposes only
+
+createXLSFile(booking);
+
+function createXLSFile(booking: Booking): void {
+    const weeks = Object.keys(booking);
+    const workers = new Set<string>();
+    const bookingData: { [worker: string]: { [weekday: string]: string } } = {};
+    weeks.forEach((week) => {
+        const weekdays = WEEKDAYS; // iterate over all weekdays
+        weekdays.forEach((weekday) => {
+            const workersList = Object.keys(booking[week][weekday] || {}); // use default empty object if no workers are assigned to this weekday
+            workersList.forEach((worker) => workers.add(worker));
+            workersList.forEach((worker) => {
+                if (!bookingData[worker]) {
+                    bookingData[worker] = {};
+                }
+                bookingData[worker][weekday] = booking[week][weekday]?.[worker] || ''; // use default empty string if worker is not assigned to this weekday
+            });
+        });
+    });
+    const workerNames = Array.from(workers.values());
+    const data = workerNames.map((worker) => {
+        const rowData = [worker];
+        WEEKDAYS.forEach((weekday) => {
+            rowData.push(bookingData[worker][weekday] || '');
+        });
+        return rowData;
+    });
+    data.unshift(['', ...WEEKDAYS]);
+    const sheet = XLSX.utils.aoa_to_sheet(data);
+    const wb = XLSX.utils.book_new();
+    weeks.forEach((week) => {
+        XLSX.utils.book_append_sheet(wb, sheet, week); // add a sheet for each week
+    });
+    const buffer = XLSX.write(wb, { bookType: 'xlsx', type: 'buffer' });
+    fs.writeFileSync('booking.xlsx', buffer);
+}
