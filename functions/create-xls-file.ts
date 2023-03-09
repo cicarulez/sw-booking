c08fc56a384f3e67e9bc0a186ef7ef6c4a3e0dd8
DC <cappadavide@gmail.com>
2023-03-09 10:43:44 +0100

    fix(xlsx): incorrectly generated identical sheets for each week

--- functions/create-xls-file.ts
+++ functions/create-xls-file.ts
@@ -7,6 +7,7 @@ export function createXLSFile(booking: Booking, config: Config): void {
     const weeks = Object.keys(booking);
     const workers = new Set<string>();
     const bookingData: { [worker: string]: { [weekday: string]: string } } = {};
+    const sheet: {[k: string]: XLSX.WorkSheet} = {};
     weeks.forEach((week) => {
         // iterate over all weekdays
         config.weekDays.forEach((weekday) => {
@@ -19,20 +20,20 @@ export function createXLSFile(booking: Booking, config: Config): void {
                 bookingData[worker][weekday] = booking[week][weekday]?.[worker] || ''; // use default empty string if worker is not assigned to this weekday
             });
         });
-    });
-    const workerNames = Array.from(workers.values());
-    const data = workerNames.map((worker) => {
-        const rowData = [worker];
-        config.weekDays.forEach((weekday) => {
-            rowData.push(bookingData[worker][weekday] || '');
+        const workerNames = Array.from(workers.values());
+        const data = workerNames.map((worker) => {
+            const rowData = [worker];
+            config.weekDays.forEach((weekday) => {
+                rowData.push(bookingData[worker][weekday] || '');
+            });
+            return rowData;
         });
-        return rowData;
+        data.unshift(['', ...config.weekDays]);
+        sheet[week] = XLSX.utils.aoa_to_sheet(data);
     });
-    data.unshift(['', ...config.weekDays]);
-    const sheet = XLSX.utils.aoa_to_sheet(data);
     const wb = XLSX.utils.book_new();
     weeks.forEach((week) => {
-        XLSX.utils.book_append_sheet(wb, sheet, week); // add a sheet for each week
+        XLSX.utils.book_append_sheet(wb, sheet[week], week); // add a sheet for each week
     });
     const buffer = XLSX.write(wb, { bookType: 'xlsx', type: 'buffer' });
     fs.writeFileSync(`booking-${new Date().getTime()}.xlsx`, buffer);
